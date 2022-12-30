b2d19c16b021d62ef04efe5cb97937addc698413
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-12-30 08:53:40 -0300

    altera imagem

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,13 @@
 import Footer from "../components/footer";
 import Header from "../components/header";
-import hortfruti from '../imgs/hortfruti.png';
+import hortfruti from '../imgs/hortfruti.jpg';
 
 function Home() {
 	return (
 		<>
 			<Header />
 			<section className="home_section">
-				<h1>Boas vindas ao Hortfruti!</h1>
+				<h1>Boas vindas ao <span>Hortfruti</span>!</h1>
 				<p>O app. onde é possível listar verduras, legumes e pesquisá-los pelo nome.</p>
 				<img src={hortfruti} alt="Hortfruti"/>
 			</section>
